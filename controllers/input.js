fce1d90f25eb9955dd18ff2557487811429669c0
Rindy Nunes <RNUNES1210@GMAIL.COM>
2018-01-30 11:38:18 -0500

    got all routes to work

--- controllers/input.js
+++ controllers/input.js
@@ -5,20 +5,13 @@ const Input = require('../db/input');
 const Clothing = require('../db/clothing');
 const Weather = require('../db/weather.js')
 
-
-// const Weather = require('../db/weather');
-
-
 //Routes for the INPUT Form
-
 router.get('/input', (req, res)=>{
-    console.log('get')
     Input.find({})
     .then(function(input){
         res.json(input)    
-    });
-})
-
+    })
+});
 
 router.post('/input', (req, res)=>{
     Input.create(req.body)
@@ -28,8 +21,6 @@ router.post('/input', (req, res)=>{
 });
 
 router.put('/input/:id', (req, res)=>{
-    console.log('put')
-    console.log(req.params.id)
     Input.findByIdAndUpdate({_id: req.params.id}, req.body)
     .then(function(input){
     res.json(input)
@@ -38,7 +29,6 @@ router.put('/input/:id', (req, res)=>{
 });
 
 router.delete('/input/:id', (req,res)=>{
-    console.log('delete')
     Input.findByIdAndRemove({_id: req.params.id}, req.body)
     .then(function(input){
     res.json(input)
@@ -46,33 +36,27 @@ router.delete('/input/:id', (req,res)=>{
 });
 
 //Routes for Clothing 
-
-//this works
 router.get('/clothing', (req, res)=>{
-    console.log('get clothing')
-    Input.find({})
+    Clothing.find({})
     .then(function(clothing){
         res.json(clothing)    
-    });
+    })
 });
 
-//this does not work
 router.get('/clothing/:name', (req, res)=>{
-    console.log('get clothing')
-    Input.findOne({name: req.params.name}, req.body)
-    .then(function(){
+    Clothing.findOne({name: req.params.name})
+    .then(function(clothingbyname){  
         res.json(clothingbyname)      
-    });
+    })
 });
 
-
-//this works
+//Routes for Weather
 router.get('/weather', (req, res)=>{
     console.log('get weather')
     Weather.find({})
     .then(function(weather){
         res.json(weather)
-    });
+    })
 });
 
 module.exports = router;
