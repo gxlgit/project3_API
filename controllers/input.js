e4790b04c341c0adc8a7bceec9addd6b9642bf28
Rindy Nunes <RNUNES1210@GMAIL.COM>
2018-01-30 10:40:32 -0500

    get route for weather works

--- controllers/input.js
+++ controllers/input.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const Input = require('../db/input');
 const Clothing = require('../db/clothing');
+const Weather = require('../db/weather.js')
+
+
 // const Weather = require('../db/weather');
 
 
@@ -11,16 +14,16 @@ const Clothing = require('../db/clothing');
 router.get('/input', (req, res)=>{
     console.log('get')
     Input.find({})
-    .then(function(clothings){
-        res.json(clothings)    
+    .then(function(input){
+        res.json(input)    
     });
 })
 
 
 router.post('/input', (req, res)=>{
     Input.create(req.body)
-    .then(function(clothings){
-    res.json(clothings)
+    .then(function(input){
+    res.json(input)
     })
 });
 
@@ -28,33 +31,42 @@ router.put('/input/:id', (req, res)=>{
     console.log('put')
     console.log(req.params.id)
     Input.findByIdAndUpdate({_id: req.params.id}, req.body)
-    .then(function(clothings){
-    res.json(clothings)
+    .then(function(input){
+    res.json(input)
         })
+ //may need to use findOne for the input form later
 });
-//may need to use findOne later
-
 
 router.delete('/input/:id', (req,res)=>{
     console.log('delete')
     Input.findByIdAndRemove({_id: req.params.id}, req.body)
-    .then(function(clothing){
-    res.json(clothing)
+    .then(function(input){
+    res.json(input)
     })
 });
 
 //Routes for Clothing 
 
+//this works
 router.get('/clothing', (req, res)=>{
     console.log('get clothing')
     Input.find({})
-    .then(function(input){
-        res.json(input)    
+    .then(function(clothing){
+        res.json(clothing)    
     });
 });
+
+//this does not work
+router.get('/clothing/:name', (req, res)=>{
+    console.log('get clothing')
+    Input.findOne({name: req.params.name}, req.body)
+    .then(function(){
+        res.json(clothingbyname)      
+    });
+});
+
+
 //this works
-
-
 router.get('/weather', (req, res)=>{
     console.log('get weather')
     Weather.find({})
